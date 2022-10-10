97deeb4e408f47e084a6d42ee7b4709afcc9787f
Mateus-Sousa23 <mateusmartinsipu2@gmail.com>
2022-10-10 16:32:43 -0300

    [add]: Creating header component

---
+++ frontend/src/components/Header/header.tsx
@@ -0,0 +1,16 @@
+import { HeaderContainer, Logo } from './Header.style';
+
+const Header = () => {
+  return (
+    <HeaderContainer>  
+        <div>  
+          <Logo src="/images/myteacher.png" />
+        </div>
+
+        <p>Econtre o professor perfeito!</p>
+    </HeaderContainer>
+    
+  )
+}
+
+export default Header;
